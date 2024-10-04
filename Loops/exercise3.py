ca6b97a2fdc4396ecf246214c296ca6cf87de358
gosupark27 <hpark41@wgu.edu>
2024-10-03 17:50:26 -0700

    completed all loop exercises

---
+++ Loops/exercise3.py
@@ -0,0 +1,5 @@
+# Write a loop that prints the value of the greeting variable three times.
+
+greeting = 'Aloha!'
+for _ in range(3):
+    print(greeting)
---
+++ Loops/exercise8.py
@@ -0,0 +1,10 @@
+# The following code keeps looping forever. (You can hit Ctrl-C to stop it.) 
+# Why does the loop keep running? Modify it so that it stops after the first iteration.
+
+# This loop keeps going indefinitely because the conditional will never evaluate to False and 
+# terminate the loop 
+while True:
+    print("and on")
+    break
+
+
