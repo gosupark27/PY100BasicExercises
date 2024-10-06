159838a7c042dcc897b42110de96c5ce1b33746e
gosupark27 <hpark41@wgu.edu>
2024-10-05 21:56:28 -0700

    completed all exercises for dictionary section

---
+++ Dictionaries/exercise1.py
@@ -0,0 +1,9 @@
+# Create a dictionary that contains the following data, and assign it to a variable named car.
+# type	color	mileage
+# sedan	blue	80000
+car = {
+      'type': 'sedan',
+      'color': 'blue',
+      'mileage': 80_000,
+}
+print(car)
---
+++ Dictionaries/exercise3.py
@@ -0,0 +1,12 @@
+# Using the following code, delete the 'mileage' key and its associated value from car.
+car = {
+    'type':    'sedan',
+    'color':   'blue',
+    'mileage': 80_000,
+    'year':    2003,
+}
+
+car.pop('mileage') 
+# Equivalent way of deleting 'mileage' key
+# del car['mileage']
+print(car)
